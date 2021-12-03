1f53e68020c45eae33ef63d6b70eb4a96a25eda2
jonnamaa <jonna1805@student.hamk.fi>
2021-12-03 16:43:08 +0200

    Initial commit

---
+++ src/AutoAppi.java
@@ -0,0 +1,64 @@
+
+public class AutoAppi {
+
+	public static void main(String[] args) {
+	
+
+	}
+
+}
+
+class Auto
+{
+	public String merkki;
+	public String malli;
+	public int bensanMaara;
+	
+	public Auto()
+	{
+		merkki = "";
+		malli = "";
+		bensanMaara =0;
+	}
+	
+	public Auto(String asetaMerkki, String asetaMalli, int asetaBensa)
+	{
+		merkki = asetaMerkki;
+		malli = asetaMalli;
+		bensanMaara = asetaBensa;
+	}
+	
+	
+	public void jarruta()
+	{
+		System.out.println("Auto jarruttaa");
+	}
+	
+	public void kiihdyta()
+	{
+		if(bensanMaara > 0) {
+			System.out.println("Auto kiihtyy");
+			bensanMaara = bensanMaara -1;
+		}
+		else
+		{
+			System.out.println("Auto ei voi kiihtyä, ei bensaa.");
+		}
+		
+	}
+	
+	public void naytaTiedot()
+	{
+		System.out.println("Merkki: " + merkki);
+		System.out.println("Malli: " + malli);
+		System.out.println("Bensan määrä: " + bensanMaara);
+	}
+	
+	public void tankkaa(int maara)
+	{
+		bensanMaara = bensanMaara + maara;
+	}
+}
+
+
+
